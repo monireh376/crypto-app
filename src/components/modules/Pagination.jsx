d4b3fe39e67bf17dc5502dd43b46d5ba0e66d4b8
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-09-12 22:16:03 +0330

    we paginated the coins data

--- src/components/modules/Pagination.jsx
+++ src/components/modules/Pagination.jsx
@@ -1,8 +1,4 @@
-import { useState } from "react";
-
-function Pagination() {
-  const [page, setPage] = useState(1);
-
+function Pagination({ page, setPage }) {
   const previousHandler = () => {
     if (page <= 1) return;
     setPage((page) => page - 1);
@@ -15,16 +11,14 @@ function Pagination() {
   return (
     <div>
       <button onClick={previousHandler}>previous</button>
-      <p style={{color: page === 1 ? "red" : "inherit"}}>1</p>
-      <p style={{color: page === 2 ? "red" : "inherit"}}>2</p>
-      {
-        page > 2 && page< 9 && (
-            <>
-            <span>...</span>
-            <p>{page}</p>
-            </>
-        )
-      }
+      <p style={{ color: page === 1 ? "red" : "inherit" }}>1</p>
+      <p style={{ color: page === 2 ? "red" : "inherit" }}>2</p>
+      {page > 2 && page < 9 && (
+        <>
+          <span>...</span>
+          <p>{page}</p>
+        </>
+      )}
       <p>...</p>
       <p>9</p>
       <p>10</p>
