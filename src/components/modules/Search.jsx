c90a1f666b89d66bdb397aed879980f563efd824
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-09-28 12:04:13 +0330

    add search mode to project

--- src/components/modules/Search.jsx
+++ src/components/modules/Search.jsx
@@ -1,16 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { searchCoin } from "../../services/cryptoApi";
 
-function Search({currency, setCurrency}) {
+function Search({ currency, setCurrency }) {
+  const [text, setText] = useState("");
+  const [coins, setCoins] = useState([]);
+
+  useEffect(() => {
+    if (!text) return;
+
+    const search = async () => {
+      const res = await fetch(searchCoin(text));
+      const json = await res.json();
+      console.log(json);
+      if(json.coins) setCoins(json.coins);
+    };
+
+    search();
+  }, [text]);
   return (
     <div>
-      <input type="text" />
+      <input
+        type="text"
+        placeholder="Search"
+        value={text}
+        onChange={(e) => setText(e.target.value)}
+      />
       <select value={currency} onChange={(e) => setCurrency(e.target.value)}>
         <option value="usd">USD</option>
         <option value="eur">EUR</option>
         <option value="jpy">JPY</option>
       </select>
     </div>
-  )
+  );
 }
 
-export default Search
+export default Search;
