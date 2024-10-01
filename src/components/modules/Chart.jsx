9a3aa768e2df49b5f4fa6734aed5e3cff7ba423b
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-10-01 23:15:11 +0330

    implement the chart

--- src/components/modules/Chart.jsx
+++ src/components/modules/Chart.jsx
@@ -1,5 +1,17 @@
 import { useState } from "react";
+
+import {
+  CartesianGrid,
+  Legend,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from "recharts";
 import { convertData } from "../../helpers/convertData";
+
 import styles from "./Chart.module.css";
 
 function Chart({ chart, setChart }) {
@@ -11,10 +23,32 @@ function Chart({ chart, setChart }) {
         x
       </span>
       <div className={styles.chart}>
-        
+        <div className={styles.graph}>
+          <ChartComponent data={convertData(chart, type)} type={type} />
+        </div>
       </div>
     </div>
   );
 }
 
 export default Chart;
+
+const ChartComponent = ({data, type}) => {
+  return (
+    <ResponsiveContainer width="100%" height="100%">
+      <LineChart width={400} height={400} data={data}>
+        <Line
+          type="monotone"
+          dataKey={type}
+          stroke="#3874ff"
+          strokeWidth="2px"
+        />
+        <CartesianGrid stroke="#404042" />
+        <YAxis dataKey={type} domain={("auto", "auto")} />
+        <XAxis dataKey={"date"} hide />
+        <Legend />
+        <Tooltip />
+      </LineChart>
+    </ResponsiveContainer>
+  );
+};
