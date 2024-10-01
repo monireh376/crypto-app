68de2a81f3b43f58962bbc0e2eee894b157817ed
Monireh Jamshidi <monireh.jamshidi@gmail.com>
2024-10-01 09:00:48 +0330

    get data chart from api

--- src/components/modules/TableCoin.jsx
+++ src/components/modules/TableCoin.jsx
@@ -1,5 +1,6 @@
 import { RotatingLines } from "react-loader-spinner";
 
+import { marketChart } from "../../services/cryptoApi";
 import chartUp from "../../assets/chart-up.svg";
 import chartDown from "../../assets/chart-down.svg";
 
@@ -42,6 +43,7 @@ export default TableCoin;
 
 const TableRow = ({
   coin: {
+    id,
     image,
     symbol,
     name,
@@ -63,8 +65,14 @@ const TableRow = ({
     }
   };
 
-  const showHandler = () => {
-    setChart(true);
+  const showHandler = async () => {
+    try {
+      const res = await fetch(marketChart(id));
+      const json = await res.json();
+      setChart(json);
+    } catch (error) {
+      setChart(null);
+    }
   };
   return (
     <tr>
